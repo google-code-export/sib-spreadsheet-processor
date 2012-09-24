6ddadd9b75a2773ff73081fd4b137e8f9e7389d2
daniel.amariles88 <daniel.amariles88@2b0edde3-8f1d-0273-a416-9537830e36c4>
2012-09-24 17:10:12 +0000

    Initial import.

---
+++ trunk/spdr/src/main/java/org/sib/sprd/App.java
@@ -0,0 +1,13 @@
+package org.sib.sprd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
